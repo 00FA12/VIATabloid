19e43859b6a2332625cdd73121d19c6ebb9f515a
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-03-12 19:59:19 +0100

    IStoruLogic created

--- Backend/Domain/Model/Department.cs
+++ Backend/Domain/Model/Department.cs
@@ -1,6 +1,7 @@
-﻿namespace Domain;
+﻿namespace Domain.Model;
 
 public class Department
 {
     public IEnumerable<Story> stories {get; set;}
+    public int id {get; set;}
 }
--- Backend/Domain/Model/Story.cs
+++ Backend/Domain/Model/Story.cs
@@ -1,7 +1,8 @@
-﻿namespace Domain;
+﻿namespace Domain.Model;
 
 public class Story
 {
     public string title {get; set;}
     public string body {get; set;}
+    public int id {get; set;}
 }
