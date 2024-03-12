4f05c45419cedabb25869c22fe9faf6693428be8
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-03-12 19:59:50 +0100

    IStoruLogic created

---
+++ Backend/Application/Logic/StoryLogic.cs
@@ -0,0 +1,27 @@
+﻿using Application.LogicInterfaces;
+using Domain.Model;
+
+namespace Application;
+
+public class StoryLogic : IStoryLogic
+{
+    public Task<Story> CreateStoryAsync(string title, string body)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Story> DeleteStoryAsync(int storyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Story>> GetAllStoriesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Story> GetStoryAsync(int storyId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Backend/Application/LogicInterfaces/IStoryLogic.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Model;
+
+namespace Application.LogicInterfaces;
+
+public interface IStoryLogic
+{   
+    Task<Story> GetStoryAsync(int storyId);
+    Task<IEnumerable<Story>> GetAllStoriesAsync();
+
+    Task<Story> CreateStoryAsync(string title, string body);
+    Task<Story> DeleteStoryAsync(int storyId);
+}
