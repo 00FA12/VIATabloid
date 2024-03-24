4977309a02c3b33deb33810e54530f469159983d
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-03-24 13:08:28 +0100

    added init.sql

---
+++ DB/init.sql
@@ -0,0 +1,22 @@
+DROP SCHEMA IF EXISTS viatabloid;
+
+CREATE SCHEMA IF NOT EXISTS viatabloid;
+SET SCHEMA 'viatabloid';
+SET search_path = viatabloid;
+
+CREATE TABLE tabloid(
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE department(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    tabloidId INT REFERENCES tabloid(id)
+);
+
+CREATE TABLE story(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    body text,
+    departmentId INT REFERENCES department(id)
+);
